d8934795629097481255ef8be10bad12cf81b0dc
mataoshou <mataoshou@163.com>
2019-06-21 14:52:23 +0800

    db
    db

---
+++ core/shineon_server_user/src/main/java/com/shineon/coder/CoderApplication.java
@@ -0,0 +1,17 @@
+package com.shineon.coder;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@MapperScan("com.shineon.coder.db.dao")
+@EnableEurekaClient
+public class CoderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoderApplication.class, args);
+    }
+
+}
---
+++ core/shineon_server_user/src/main/java/com/shineon/coder/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.shineon.coder.controller;
+
+import com.shineon.coder.db.dao.ShineonUserMapper;
+import com.shineon.coder.db.pojo.ShineonUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    ShineonUserMapper mapper;
+
+    @RequestMapping("/get")
+    public ShineonUser get(int id)
+    {
+        return mapper.selectByPrimaryKey(id);
+    }
+
+}
