6a1b6a22f3f26d8fc6bde1c4b372d1e839dbd6ec
mataoshou <mataoshou@163.com>
2019-07-22 10:28:08 +0800

    web
    web

--- core/shineon_db_user/src/main/java/com/shineon/coder/controller/UserController.java
+++ core/shineon_db_user/src/main/java/com/shineon/coder/controller/UserController.java
@@ -3,6 +3,8 @@ package com.shineon.coder.controller;
 import com.alibaba.fastjson.JSON;
 import com.shineon.coder.db.dao.ShineonUserMapper;
 import com.shineon.coder.db.pojo.ShineonUser;
+import com.shineon.coder.nodb.dao.GeneralDao;
+import com.shineon.coder.nodb.pojo.GenerallItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 @RestController
@@ -20,6 +23,9 @@ public class UserController {
 
     @Autowired
     ShineonUserMapper shineonUserMapper;
+
+    @Autowired
+    GeneralDao generalDao;
 
     @RequestMapping("/getUser")
     public ShineonUser get(int id)
@@ -40,6 +46,42 @@ public class UserController {
         }
 
         System.out.println( redisTemplate.opsForValue().get("user").toString());
+
+
+        GenerallItem item = new GenerallItem();
+        item.setContent("111111111111111" +"2aaaaa2222");
+
+        item.setType(100);
+        generalDao.insert(item);
+
+
+        System.out.println(item.getId());
+
+        List<GenerallItem> list = generalDao.findall(GenerallItem.class);
+
+        for(GenerallItem item1: list)
+        {
+            System.out.println("........"+item1.getId() +"....." +item1.getType() +"...." +item1.getContent());
+        }
+
+
+
+        List<GenerallItem> list1 = generalDao.findall("2222");
+
+        for(GenerallItem item1: list1)
+        {
+            System.out.println("111111111........"+item1.getId() +"....." +item1.getType()+"...." +item1.getContent());
+        }
+
+
+        List<GenerallItem> list2 = generalDao.findall(0);
+
+
+        for(GenerallItem item1: list2)
+        {
+            System.out.println("2222222........"+item1.getId() +"....." +item1.getType()+"...." +item1.getContent());
+        }
+
 
         return user;
 //        return null;
---
+++ core/shineon_db_user/src/main/java/com/shineon/coder/nodb/pojo/GenerallItem.java
@@ -0,0 +1,81 @@
+package com.shineon.coder.nodb.pojo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "general")
+public class GenerallItem {
+
+    @Id
+    private String id;
+
+    private String title;
+
+    private int type;
+
+    private String content;
+
+    private int creator;
+
+    private String displayName;
+
+    private  long timeCreated;
+
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getCreator() {
+        return creator;
+    }
+
+    public void setCreator(int creator) {
+        this.creator = creator;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public long getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(long timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+}
