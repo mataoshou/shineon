c18a45541cdf9b9f1e9a38cb6a8e53b18056ae58
mataoshou <mataoshou@163.com>
2019-10-08 14:17:51 +0800

    目录结构调整
    目录结构调整

--- core/mg_db_user/src/main/java/com/mg/coder/convert/util/ShineonUserAttachedCommonUtil.java
+++
@@ -1,9 +0,0 @@
-//package com.mg.coder.convert.util;
-//
-//import com.mg.coder.convert.base.ShineonUserAttachedCommonBase;;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class ShineonUserAttachedCommonUtil extends ShineonUserAttachedCommonBase {
-//
-//}
--- core/mg_db_user/src/main/java/com/mg/coder/convert/util/ShineonUserCommonUtil.java
+++
@@ -1,9 +0,0 @@
-//package com.mg.coder.convert.util;
-//
-//import com.mg.coder.convert.base.ShineonUserCommonBase;;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class ShineonUserCommonUtil extends ShineonUserCommonBase {
-//
-//}
